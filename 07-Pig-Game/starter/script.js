3d6e34c7ec727700d8693d277987520d7ef9b224
rahilsama <rahil.sama@ucalgary.ca>
2023-02-01 10:52:07 -0700

    82. PROJECT#3 pig gamae: init the screen vals

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -1,1 +1,15 @@
 'use strict';
+
+let p1Score = 0;
+let p2Score = 0;
+
+const score1 = document.getElementById('score--0');
+const score2 = document.getElementById('score--1');
+
+//Setting score to the zero when the game starts
+score1.textContent = 0;
+score2.textContent = 0;
+
+//hiding the dice when first starting the game using its class
+const dice = document.querySelector('.dice');
+dice.classList.add('hidden');
