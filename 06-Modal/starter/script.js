144e3df29eba5b53b73fdaa23faf31fc690d8366
rahilsama <rahil.sama@ucalgary.ca>
2023-02-01 09:37:27 -0700

    80. playing around wiht css classes using js

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -3,9 +3,26 @@
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnShowModal = document.querySelectorAll('.show-modal');
+const xBtn = document.querySelector('.close-modal');
 
 //console.log(btnShowModal);
+const showModal = function () {
+  console.log('button clicked');
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
 
-for (let i = 0; i < btnShowModal.length; i++) {
-  console.log(btnShowModal[i].textContent);
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+for (const element of btnShowModal) {
+  element.addEventListener('click', showModal);
 }
+
+xBtn.addEventListener('click', closeModal);
+
+overlay.addEventListener('click', closeModal);
+
+//Functions
