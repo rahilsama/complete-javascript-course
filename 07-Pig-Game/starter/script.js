ac22935fc3ff939cf638595ffd1cd7780283f6e8
Rahil Sama <rahil.sama@ucalgary.ca>
2023-02-04 17:14:34 -0700

    finished the Pig game

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -2,6 +2,9 @@
 
 let currentScore = 0;
 let currentPlayer = 0;
+let playing = true;
+
+let scores = [0, 0];
 
 const score0 = document.getElementById('score--0');
 const score1 = document.getElementById('score--1');
@@ -9,8 +12,8 @@ const current0EL = document.getElementById('current--0');
 const current1EL = document.getElementById('current--1');
 
 //Setting score to the zero when the game starts
-score0.textContent = 0;
-score1.textContent = 0;
+score0.textContent = scores[0];
+score1.textContent = scores[1];
 
 //hiding the dice when first starting the game using its class
 const dice = document.querySelector('.dice');
@@ -25,67 +28,100 @@ const btnNew = document.querySelector('.btn--new');
 const btnHold = document.querySelector('.btn--hold');
 
 btnRoll.addEventListener('click', function () {
-  console.log('Roll button clicked');
-  //the variable randNUM calculates a random number between 1 and 6 (inclusive) which will be used to find out which dice will be shown and
-  //calculate the score accordingly
-  let diceRoll = Math.trunc(Math.random() * 6) + 1;
-  console.log('Random number: ' + diceRoll);
+  if (playing) {
+    //calculate the score accordingly
+    let diceRoll = Math.trunc(Math.random() * 6) + 1;
+    console.log('Random number: ' + diceRoll);
 
-  //Display the dice roll according random dice roll
-  dice.classList.remove('hidden');
-  dice.src = `dice-${diceRoll}.png`;
+    //Display the dice roll according random dice roll
+    dice.classList.remove('hidden');
+    dice.src = `dice-${diceRoll}.png`;
 
-  //Check for rolled 1
-  if (diceRoll !== 1) {
-    //Add the rolled score to the player's current score
-    currentScore += diceRoll;
-    if (currentPlayer === 0) {
-      current0EL.textContent = currentScore;
+    //Check for rolled 1
+    if (diceRoll !== 1) {
+      //the variable randNUM calculates a random number between 1 and 6 (inclusive) which will be used to find out which dice will be shown and
+      //Add the rolled score to the player's current score
+      currentScore += diceRoll;
+      if (currentPlayer === 0) {
+        current0EL.textContent = currentScore;
+      } else {
+        current1EL.textContent = currentScore;
+      }
     } else {
-      current1EL.textContent = currentScore;
-    }
-  } else {
-    //Reset player's current score to 0 and change player
-    currentScore = 0;
+      //Reset player's current score to 0 and change player
+      currentScore = 0;
 
-    if (currentPlayer == 0) {
-      currentPlayer = 1;
-      current0EL.textContent = currentScore;
-      //swap the active player color
-      player0.classList.remove('player--active');
-      player1.classList.add('player--active');
-    } else {
-      currentPlayer = 0;
-      current1EL.textContent = currentScore;
-      //swap the active player color
-      player1.classList.remove('player--active');
-      player0.classList.add('player--active');
-    }
+      if (currentPlayer == 0) {
+        currentPlayer = 1;
+        current0EL.textContent = currentScore;
+        //swap the active player color
+        player0.classList.toggle('player--active');
+        player1.classList.toggle('player--active');
+      } else {
+        currentPlayer = 0;
+        current1EL.textContent = currentScore;
+        //swap the active player color
+        player1.classList.toggle('player--active');
+        player0.classList.toggle('player--active');
+      }
 
-    //Change the score side
+      //Change the score side
+    }
   }
 });
 
 btnNew.addEventListener('click', function () {
   //reset the game and everything
+  currentScore = 0;
+  currentPlayer = 0;
+  playing = true;
+  scores = [0, 0];
+
+  //Setting score to the zero when the game starts
+  score0.textContent = scores[0];
+  score1.textContent = scores[1];
+
+  //hiding the dice when first starting the game using its class
+  const dice = document.querySelector('.dice');
+  dice.classList.add('hidden');
+
+  player0.classList.remove('player--winner');
+  player1.classList.remove('player--winner');
+  player1.classList.remove('player--active');
+  player0.classList.remove('player--active');
+  player0.classList.add('player--active');
 });
 
 btnHold.addEventListener('click', function () {
-  currentScore = 0;
-  if (currentPlayer === 0) {
-    score0.textContent = current0EL.textContent;
-    currentPlayer = 1;
-    current0EL.textContent = currentScore;
-    //swap the active player color
-    player0.classList.remove('player--active');
-    player1.classList.add('player--active');
-  } else {
-    score1.textContent = current1EL.textContent;
-    currentPlayer = 0;
-    current1EL.textContent = currentScore;
-    //swap the active player color
-    player1.classList.remove('player--active');
-    player0.classList.add('player--active');
+  if (playing) {
+    scores[currentPlayer] += currentScore;
+
+    currentScore = 0;
+    if (currentPlayer === 0) {
+      score0.textContent = scores[currentPlayer];
+      if (scores[currentPlayer] >= 10) {
+        playing = false;
+        player0.classList.toggle('player--active');
+        player0.classList.toggle('player--winner');
+      }
+      currentPlayer = 1;
+      current0EL.textContent = currentScore;
+      //swap the active player color
+      player0.classList.toggle('player--active');
+      player1.classList.toggle('player--active');
+    } else {
+      score1.textContent = scores[currentPlayer];
+      if (scores[currentPlayer] >= 10) {
+        playing = false;
+        player1.classList.toggle('player--active');
+        player1.classList.toggle('player--winner');
+      }
+      currentPlayer = 0;
+      current1EL.textContent = currentScore;
+      //swap the active player color
+      player1.classList.toggle('player--active');
+      player0.classList.toggle('player--active');
+    }
   }
 });
 //
